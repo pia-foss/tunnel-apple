1758497f118eb540e887170cbd5ddb5e989efb55
Davide De Rosa <keeshux@gmail.com>
2018-04-15 19:05:57 +0200

    Implement build/parse for raw packet streams
    Prepend 16-bit (NBO) packet length in raw stream mode.

--- PIATunnel/Sources/Core/Packet.swift
+++ PIATunnel/Sources/Core/Packet.swift
@@ -42,6 +42,40 @@ class ControlPacket: Packet {
     let payload: Data?
     
     var sentDate: Date?
+
+    static func parsed(_ stream: Data) -> (Int, [Data]) {
+        var ni = 0
+        var parsed: [Data] = []
+        while (ni + 2 <= stream.count) {
+            let networkPacklen = stream.UInt16Value(from: ni)
+            let packlen = Int(CFSwapInt16BigToHost(networkPacklen))
+            let start = ni + 2
+            let end = start + packlen
+            guard (end <= stream.count) else {
+                break
+            }
+            let packet = stream.subdata(offset: start, count: end - start)
+            parsed.append(packet)
+            ni = end
+        }
+        return (ni, parsed)
+    }
+    
+    static func stream(_ packet: Data) -> Data {
+        var stream = Data(capacity: 2 + packet.count)
+        stream.append(UInt16(packet.count).bigEndian)
+        stream.append(contentsOf: packet)
+        return stream
+    }
+    
+    static func stream(_ packets: [Data]) -> Data {
+        var raw = Data()
+        for payload in packets {
+            raw.append(UInt16(payload.count).bigEndian)
+            raw.append(payload)
+        }
+        return raw
+    }
     
     init(_ packetId: UInt32, _ code: PacketCode, _ key: UInt8, _ sessionId: Data?, _ payload: Data?) {
         self.packetId = packetId

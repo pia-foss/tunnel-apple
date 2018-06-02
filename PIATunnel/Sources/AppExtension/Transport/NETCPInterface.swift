3f95d250e8c846acea27b2c9b7d3320a2c78f16b
Davide De Rosa <keeshux@gmail.com>
2018-06-03 00:57:54 +0200

    Merge transport implementations
    Implement GenericSocket and LinkInterface in the same objects.
    
    - GenericSocket is the AppExtension side
    - LinkInterface is the SessionProxy side

--- PIATunnel/Sources/AppExtension/Transport/NETCPInterface.swift
+++ PIATunnel/Sources/AppExtension/Transport/NETCPInterface.swift
@@ -8,23 +8,148 @@
 
 import Foundation
 import NetworkExtension
+import SwiftyBeaver
 
-class NETCPInterface: LinkInterface {
+private let log = SwiftyBeaver.self
+
+class NETCPInterface: NSObject, GenericSocket, LinkInterface {
+    private static var linkContext = 0
+    
     private let impl: NWTCPConnection
     
     private let maxPacketSize: Int
+
+    init(impl: NWTCPConnection, maxPacketSize: Int = 32768) {
+        self.impl = impl
+        self.maxPacketSize = maxPacketSize
+        endpoint = impl.endpoint
+        isActive = false
+    }
+    
+    // MARK: GenericSocket
+    
+    private weak var queue: DispatchQueue?
+    
+    private var isActive: Bool
+    
+    let endpoint: NWEndpoint
+    
+    var remoteAddress: String? {
+        return (impl.remoteAddress as? NWHostEndpoint)?.hostname
+    }
+    
+    var hasBetterPath: Bool {
+        return impl.hasBetterPath
+    }
+    
+    weak var delegate: GenericSocketDelegate?
+    
+    func observe(queue: DispatchQueue, activeTimeout: Int) {
+        isActive = false
+        
+        self.queue = queue
+        queue.schedule(after: .milliseconds(activeTimeout)) { [weak self] in
+            guard let isActive = self?.isActive else {
+                return
+            }
+            guard isActive else {
+                log.debug("Socket timed out waiting for activity, cancelling...")
+                self?.impl.cancel()
+                return
+            }
+        }
+        impl.addObserver(self, forKeyPath: #keyPath(NWTCPConnection.state), options: [.initial, .new], context: &NETCPInterface.linkContext)
+        impl.addObserver(self, forKeyPath: #keyPath(NWTCPConnection.hasBetterPath), options: .new, context: &NETCPInterface.linkContext)
+    }
+    
+    func unobserve() {
+        impl.removeObserver(self, forKeyPath: #keyPath(NWTCPConnection.state), context: &NETCPInterface.linkContext)
+        impl.removeObserver(self, forKeyPath: #keyPath(NWTCPConnection.hasBetterPath), context: &NETCPInterface.linkContext)
+    }
+    
+    func shutdown() {
+        impl.writeClose()
+        impl.cancel()
+    }
+    
+    func upgraded() -> GenericSocket? {
+        guard impl.hasBetterPath else {
+            return nil
+        }
+        return NETCPInterface(impl: NWTCPConnection(upgradeFor: impl))
+    }
+    
+    func link() -> LinkInterface {
+        return self
+    }
+    
+    // MARK: Connection KVO (any queue)
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        guard (context == &NETCPInterface.linkContext) else {
+            super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
+            return
+        }
+//        if let keyPath = keyPath {
+//            log.debug("KVO change reported (\(anyPointer(object)).\(keyPath))")
+//        }
+        queue?.async {
+            self.observeValueInTunnelQueue(forKeyPath: keyPath, of: object, change: change, context: context)
+        }
+    }
+    
+    private func observeValueInTunnelQueue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+//        if let keyPath = keyPath {
+//            log.debug("KVO change reported (\(anyPointer(object)).\(keyPath))")
+//        }
+        guard let impl = object as? NWTCPConnection, (impl == self.impl) else {
+            log.warning("Discard KVO change from old socket")
+            return
+        }
+        guard let keyPath = keyPath else {
+            return
+        }
+        switch keyPath {
+        case #keyPath(NWTCPConnection.state):
+            if let resolvedEndpoint = impl.remoteAddress {
+                log.debug("Socket state is \(impl.state) (endpoint: \(impl.endpoint) -> \(resolvedEndpoint))")
+            } else {
+                log.debug("Socket state is \(impl.state) (endpoint: \(impl.endpoint) -> in progress)")
+            }
+            
+            switch impl.state {
+            case .connected:
+                isActive = true
+                delegate?.socketDidBecomeActive(self)
+                
+            case .cancelled:
+                delegate?.socket(self, didShutdownWithFailure: false)
+                
+            case .disconnected:
+                delegate?.socket(self, didShutdownWithFailure: true)
+                
+            default:
+                break
+            }
+            
+        case #keyPath(NWTCPConnection.hasBetterPath):
+            guard impl.hasBetterPath else {
+                break
+            }
+            log.debug("Socket has a better path")
+            delegate?.socketHasBetterPath(self)
+            
+        default:
+            break
+        }
+    }
+
+    // MARK: LinkInterface
     
     var isReliable: Bool {
         return true
     }
 
-    var remoteAddress: String? {
-        guard let endpoint = impl.remoteAddress as? NWHostEndpoint else {
-            return nil
-        }
-        return endpoint.hostname
-    }
-    
     var mtu: Int {
         return .max
     }
@@ -39,11 +164,6 @@ class NETCPInterface: LinkInterface {
     
     var hardResetTimeout: TimeInterval {
         return 5.0
-    }
-    
-    init(impl: NWTCPConnection, maxPacketSize: Int = 32768) {
-        self.impl = impl
-        self.maxPacketSize = maxPacketSize
     }
     
     func setReadHandler(_ handler: @escaping ([Data]?, Error?) -> Void) {
